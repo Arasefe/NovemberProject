420e4e8727d9c4577dc65182599ceac5a28aff80
Arasefe <ismail_yildirim69@yahoo.com>
2020-11-01 10:03:36 -0600

    Some changes added to Batman

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,6 +1,10 @@
 public class Batman {
     public static void main(String[] args) {
         System.out.println("Batman saves the city");
-        //The city is Gossum
+        //The city is Gotham
+
+    }
+    public static void batmanMethod(){
+        System.out.println("Batman saves Gotham");
     }
 }
