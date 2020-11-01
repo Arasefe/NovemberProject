3a02aa5cbe11c00e63fb8c9a6f0822535c8e102e
Arasefe <ismail_yildirim69@yahoo.com>
2020-11-01 10:39:58 -0600

    Some more changes added to Superman

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -9,5 +9,9 @@ public class Superman {
 
     public static void superMethod2(){
         System.out.println("Superman is working hard...");
+
+    }
+    public static void superMethod3(){
+        System.out.println("Superman is about to save the world");
     }
 }
