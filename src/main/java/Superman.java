33e88833a1a01a675ca1dbc2ffda9f93f581fd88
Arasefe <ismail_yildirim69@yahoo.com>
2020-11-01 10:00:37 -0600

    Some changes added to Superman

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -1,5 +1,9 @@
 public class Superman {
     public static void main(String[] args) {
         System.out.println("Superman");
+
+    }
+    public static void superMethod(){
+        System.out.println("Omg wait a second Superman saves the world!!!");
     }
 }
--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,5 +1,6 @@
 public class Batman {
     public static void main(String[] args) {
-
+        System.out.println("Batman saves the city");
+        //The city is Gossum
     }
 }
